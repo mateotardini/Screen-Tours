77d28c0658a275d8b7f6934523673d834093683a
mateotardini <mateotardini@gmail.com>
2024-06-06 17:35:32 -0300

    Finalizacion pagina de Contacto

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -13,7 +13,7 @@ function Contact() {
     return (
         <main>
             <NavBar />
-            <section style={{ minHeight: "unset",height: "43vh", position: "relative", overflow: "hidden" }}>
+            <section style={{ minHeight: "unset", height: "43vh", position: "relative", overflow: "hidden" }}>
                 <div className="videoBackground">
                     <video autoPlay muted loop playsInline>
                         <source src="/videos/Tulipans.mp4" type="video/mp4" />
@@ -34,16 +34,27 @@ function Contact() {
                 />
             </section>
 
-            <section className="sectionPaddingLeft row colorBlack" style={{ paddingTop: "0", justifyContent: "flex-start" }}>
+            <section className="sectionPaddingLeft row colorBlack" style={{ paddingTop: "0", justifyContent: "flex-start",  marginBottom:"-8%" }}>
                 <RetrowaveItem
                     src="/Retrowave7.png"
                     alt="Screen Tours Logo"
                     width="20vw"
-                    translateX="-73%"
+                    translateX="170%"
                     translateY="13%"
                     filter="none"
                     opacity={0.5}
                 />
+
+                <RetrowaveItem
+                    src="/Mill2.png"
+                    alt="Screen Tours Logo"
+                    width="23vw"
+                    translateX="138%"
+                    translateY="45%"
+                    filter="drop-shadow(6px 6px 2px #ffffff) invert(27%) sepia(82%) saturate(420%) hue-rotate(173deg) brightness(94%) contrast(96%)"
+                    opacity={1}
+                />
+
 
                 <div className="textContainer">
                     <h1 className="fontMontserrat">
@@ -54,25 +65,23 @@ function Contact() {
                     </p>
                     <div className={styles.column}>
                         <ul className={styles.links}>
-                            <li><FaMapMarkerAlt className={styles.icon} />Amsterdam, Paises Bajos</li>
-                            <li><FaClock className={styles.icon} />09:00h - 17:00h</li>
-                            <li><FaPhoneAlt className={styles.icon} /><a href="https://wa.me/5491163733844" target="_blank" rel="noreferrer" >+54 9 11 6373-3844 </a></li>
-                            <li><FaWhatsapp className={styles.icon} /><a href="https://wa.me/5491163733844" target="_blank" rel="noreferrer" >+54 9 11 6373-3844 </a></li>
-                            <li><FaEnvelope className={styles.icon} /><a href="mailto:screentours.uptodate@gmail.com" target="_blank" rel="noreferrer" >screentours.uptodate@gmail.com</a></li>
+                            <li><FaMapMarkerAlt className={styles.icon} /> <strong>Amsterdam, Paises Bajos</strong></li>
+                            <li><FaClock className={styles.icon} /><strong>09:00h - 17:00h</strong></li>
+                            <li><FaPhoneAlt className={styles.icon} /><a href="https://wa.me/5491163733844" target="_blank" rel="noreferrer" ><strong>+54 9 11 6373-3844</strong> </a></li>
+                            <li><FaWhatsapp className={styles.icon} /><a href="https://wa.me/5491163733844" target="_blank" rel="noreferrer" ><strong>+54 9 11 6373-3844</strong> </a></li>
+                            <li><FaEnvelope className={styles.icon} /><a href="mailto:screentours.uptodate@gmail.com" target="_blank" rel="noreferrer" ><strong>screentours.uptodate@gmail.com</strong></a></li>
                         </ul>
                     </div>
                 </div>
-                <div className="textContainer">
-                    <ContactForm></ContactForm>
-                </div>
+                <ContactForm></ContactForm>
             </section>
 
             <iframe
                 title="Google Maps"
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d77979.66565661412!2d4.8215604995263925!3d52.354741843979895!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c63fb5949a7755%3A0x6600fd4cb7c0af8d!2sAmsterdam%2C%20Netherlands!5e0!3m2!1sen!2sar!4v1717436473704!5m2!1sen!2sar"
-                width="100%"
-                height="400vh"
-                style={{ border: 0, position: "relative", border: "0px", top: "50%", left: "50%", transform: "translate(-50%, 28%)" }}
+                width="15%"
+                height="70%"
+                style={{ border: 0, position: "absolute", border: "0px", top: "50%", left: "50%", transform: "translate(-334%, 0%)", borderRadius: "0 45px 45px 0px"}}
                 loading="lazy"
             ></iframe>
 
